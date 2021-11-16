b34abae65a1ab5efd801a2d07f1ea9a9c88ece73
Ander Rodriguez <anderrod01@gmail.com>
2021-11-16 09:53:04 +0100

    Dialogos

--- EjercicioDialogos/app/src/main/java/com/example/ejerciciodialogos/FragmentLista.java
+++ EjercicioDialogos/app/src/main/java/com/example/ejerciciodialogos/FragmentLista.java
@@ -1,58 +1,89 @@
 package com.example.ejerciciodialogos;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class FragmentLista extends Fragment {
 
-    private Menu [] datos = new Menu[] {}
+    private Menu [] datos = new Menu[] {};
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
+    private ListView lstMenu;
+    private MenuListener listener;
 
     public FragmentLista() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentLista.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentLista newInstance(String param1, String param2) {
-        FragmentLista fragment = new FragmentLista();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        lstMenu = (ListView) getView().findViewById(R.id.lstMenu);
+        lstMenu.setAdapter(new AdaptadorMenu(this));
+
+        lstMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int position, long id) {
+                if (listener != null)
+                    listener.onMenuSeleccionado(
+                            (Menu) lstMenu.getAdapter().getItem(position));
+            }
+        });
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lista, container, false);
+    }
+
+
+    class AdaptadorMenu extends ArrayAdapter<Menu> {
+        private Activity context;
+
+        public AdaptadorMenu (Fragment context){
+            super (context.getActivity(), R.layout.listitem_menu, datos);
+            this.context = context.getActivity();
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            View item = inflater.inflate(R.layout.listitem_menu, null);
+
+            TextView lblTituloLibro = (TextView) item.findViewById(R.id.lblMenu);
+            lblTituloLibro.setText(datos[position].getNombre());
+
+            ImageView imgPortada = (ImageView) item.findViewById(R.id.img);
+            imgPortada.setImageDrawable(getResources().getDrawable(datos[position].getImg()));
+
+            return item;
+        }
+    }
+
+    public void setMenuListener (MenuListener listener){
+        this.listener=listener;
     }
 }
---
+++ EjercicioDialogos/app/src/main/java/com/example/ejerciciodialogos/MenuListener.java
@@ -0,0 +1,5 @@
+package com.example.ejerciciodialogos;
+
+public interface MenuListener {
+    void onMenuSeleccionado(Menu m);
+}
