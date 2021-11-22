62d16390821678b53247d3305a5a0a1cf5a0fc44
Ander Rodriguez <anderrod01@gmail.com>
2021-11-22 12:38:14 +0100

    lista funciona

--- EjercicioDialogos/app/src/main/java/com/example/ejerciciodialogos/ActividadMenus.java
+++ EjercicioDialogos/app/src/main/java/com/example/ejerciciodialogos/ActividadMenus.java
@@ -4,27 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class ActividadMenus extends AppCompatActivity {
 
-    private Menu[] datos;
+    private Menu[] datosCarne;
+    private Menu[] datosVeg;
 
     private ListView lstCarne;
     private ListView lstVegetariano;
@@ -52,13 +48,12 @@ public class ActividadMenus extends AppCompatActivity {
         ImageView silco = new ImageView(this);
         silco.setImageResource(R.drawable.silco);
 
-        datos = new Menu[]{new Menu("Menu Jayce", R.string.menuJayce, jayce),
+        datosCarne = new Menu[]{new Menu("Menu Jayce", R.string.menuJayce, jayce),
                 new Menu("Menu Jinx", R.string.menuJinx, jinx),
-                new Menu("Menu Vi", R.string.menuVi,vi),
-                new Menu("Menu Vander", R.string.menuVander, vander),
+                new Menu("Menu Vi", R.string.menuVi,vi)};
+
+        datosVeg =new Menu[]{ new Menu("Menu Vander", R.string.menuVander, vander),
                 new Menu("Menu Silco", R.string.menuSilco,silco)};
-
-
 
         ViewPager viewPager= findViewById(R.id.viewpager);
         viewPager.setAdapter(new PageAdapter());
@@ -76,8 +71,8 @@ public class ActividadMenus extends AppCompatActivity {
 
     class PageAdapter extends PagerAdapter {
 
-        private ConstraintLayout tab1;
-        private ConstraintLayout tab2;
+        private LinearLayout tab1;
+        private LinearLayout tab2;
         private final String[] TABS = {"Menus con carne", "Menus Vegetarianos"};
 
         @Override
@@ -96,25 +91,33 @@ public class ActividadMenus extends AppCompatActivity {
         @Override
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
             View page;
+
+
+
             switch(position){
-                case 1:
+                case 0:
 
                     if (tab1 == null) {
-                        tab1 = (ConstraintLayout) LayoutInflater.from(ActividadMenus.this).inflate(R.layout.tab1, container, false);
+                        tab1 = (LinearLayout) LayoutInflater.from(ActividadMenus.this).inflate(R.layout.tab1, container, false);
 
                     }
                     page = tab1;
-                    AdaptadorCarne adaptadorCarne = new AdaptadorCarne(tab1.getContext(), datos);
+                    AdaptadorMenus adaptadorCarne = new AdaptadorMenus(tab1.getContext(), datosCarne);
 
-                    lstCarne = tab1.findViewById(R.id.lstCarne);
+                    lstCarne = (ListView) tab1.findViewById(R.id.lstCarne);
 
                     lstCarne.setAdapter(adaptadorCarne);
                     break;
                 default:
                     if (tab2 == null) {
-                        tab2 = (ConstraintLayout) LayoutInflater.from(ActividadMenus.this).inflate(R.layout.tab2, container, false);
+                        tab2 = (LinearLayout) LayoutInflater.from(ActividadMenus.this).inflate(R.layout.tab2, container, false);
                     }
                     page = tab2;
+                    AdaptadorMenus adaptadorMenus = new AdaptadorMenus(tab2.getContext(), datosVeg);
+
+                    lstVegetariano = (ListView) tab2.findViewById(R.id.lstVegetariana);
+
+                    lstVegetariano.setAdapter(adaptadorMenus);
                     break;
             }
             container.addView(page, 0);
@@ -123,7 +126,7 @@ public class ActividadMenus extends AppCompatActivity {
 
         @Override
         public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-            return false;
+            return view == object;
         }
         @Override
         public void destroyItem(@NonNull ViewGroup container,
