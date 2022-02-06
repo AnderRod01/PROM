c0c58aa5bda86023651638a69bf7d875f91d9ff4
Ander <anderrod01@gmail.com>
2022-02-06 13:02:32 +0100

    xml 1 terminado

---
+++ EV2/EjercicioXML/app/src/main/java/com/example/ejercicioxml2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ejercicioxml2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btnEjercicio1);
+        Button btn2 = findViewById(R.id.btnEjercicio2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Ejercicio1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
